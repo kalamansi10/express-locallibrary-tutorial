c65e8db4ec1b60b406a3cb3776e4e8bd7bddc036
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-03-26 23:39:48 +0800

    Added views and controller views setup

--- models/author.js
+++ models/author.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon");
+
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   family_name: { type: String, required: true, maxLength: 100 },
@@ -25,6 +27,19 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+   const lifespan = DateTime.fromJSDate(this.date_of_death).diff(DateTime.fromJSDate(this.date_of_birth), 'years').toObject().years
+   return this.date_of_death && this.date_of_birth ? Math.abs(lifespan).toFixed(0).toString() : '';
 });
 
 // Export model
